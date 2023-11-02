3bfb6c010a9f7243ffbafff84237b2b0991cb46d
Yahahah wahyu A5 <m.nuhksp@gmail.com>
2023-11-02 20:50:04 +0700

    2nd commit

--- login.php
+++ login.php
@@ -34,9 +34,7 @@ if (isset($_POST['proses'])) {
     <title>Login - POS KEDAI RUNCIT</title>
 
     <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
-    <link
-        href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
-        rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
 
     <link href="sb-admin/css/sb-admin-2.min.css" rel="stylesheet">
 </head>
@@ -51,19 +49,16 @@ if (isset($_POST['proses'])) {
 
                         <div class="p-5">
                             <div class="text-center">
-                                <h4 class="h4 text-gray-900 mb-4"><b>Login Ke Manajemen KEDAI RUNCIT</b></h4>
+                                <h4 class="h4 text-gray-900 mb-4"><b>LOGIN KE MANAJEMEN KEDAI RUNCIT</b></h4>
                             </div>
                             <form class="form-login" method="POST">
                                 <div class="form-group">
-                                    <input type="text" class="form-control form-control-user" name="user"
-                                        placeholder="User ID" autofocus>
+                                    <input type="text" class="form-control form-control-user" name="user" placeholder="User ID" autofocus>
                                 </div>
                                 <div class="form-group">
-                                    <input type="password" class="form-control form-control-user" name="pass"
-                                        placeholder="Password">
+                                    <input type="password" class="form-control form-control-user" name="pass" placeholder="Password">
                                 </div>
-                                <button class="btn btn-primary btn-block" name="proses" type="submit"><i
-                                        class="fa fa-lock"></i>
+                                <button class="btn btn-primary btn-block" name="proses" type="submit"><i class="fa fa-lock"></i>
                                     SIGN IN</button>
                             </form>
                             <!-- <hr>
